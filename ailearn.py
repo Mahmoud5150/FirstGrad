da5ea5bbb0f27aa4a1a243c351b688444d3883ff
Mahmoud5150 <mahmoudbahrani103@gmail.com>
2025-10-13 16:51:46 +0300

    Random Tensors

--- ailearn.py
+++ ailearn.py
@@ -1,4 +1,6 @@
-import torch 
+import torch
+import numpy as np
+
 
 x = torch.tensor(2.0, requires_grad=True)
 
@@ -6,3 +8,6 @@ y = x**2+x*3+4
 
 y.backward()
 print(x.grad)
+
+randomTensor = torch.rand(3,3)
+print(randomTensor)
